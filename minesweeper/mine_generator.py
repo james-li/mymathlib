b8ac19966d0e616d324d967700428f177a3d619d
lijun <lijun01@hfitech.com>
2023-01-10 11:08:56 +0800

    dev temp 0110

--- minesweeper/mine_generator.py
+++ minesweeper/mine_generator.py
@@ -12,7 +12,7 @@ def neighbor_area(clicked_x: int, clicked_y: int, width: int, height: int) -> li
 def expand_area(arr: np.ndarray, mines: dict, clicked_x: int, clicked_y: int, blank_area: dict = {}) -> dict:
     if mines[(clicked_x, clicked_y)] != 0:
         return blank_area
-    neighbor = neighbor_area(arr, clicked_x, clicked_y)
+    neighbor = neighbor_area(clicked_x, clicked_y, arr.shape[1], arr.shape[0])
     for x, y in neighbor:
         if (x, y) != (clicked_x, clicked_y):
             if (x, y) not in blank_area:
@@ -56,47 +56,48 @@ def generate_mine_map(width: int, height: int, mine_number: int, clicked_x: int 
 WHITE = '\033[0m'
 RED = '\033[91m'
 GRAY = '\033[37m'
+BLUE = '\033[94m'
 
 
 def colored_mine(mines, blank_area, x, y):
     if (x, y) in blank_area:
-        return WHITE + str(mines[(x, y)])
+        return WHITE + "%02d" % (mines[(x, y)])
     elif mines[(x, y)] == 9:
-        return RED + str(mines[(x, y)])
+        return RED + "%02d" % (mines[(x, y)])
     else:
-        return GRAY + str(mines[(x, y)])
+        return GRAY + "%02d" % (mines[(x, y)])
 
 
-def miner_sweeper(mines, width, height, init_blank_area: dict):
-    revealed_area = {}
-    for p in init_blank_area.keys():
-        revealed_area[p] = (init_blank_area[p], 0)  # (mine_number, revealed_number)
+def mine_sweeper(mines, width, height, init_blank_area: dict):
+    revealed_area = {x: (mines[x], 0) for x in init_blank_area.keys()}
     while True:
         investigate_area = {}
-        for p in revealed_area.keys():
+        for p in list(revealed_area.keys()):
             mine_number, revealed_number = revealed_area[p]
             if mine_number == revealed_number:  # all mines revealed
                 continue
             x, y = p
             neighbor = neighbor_area(x, y, width, height)
+            revealed_mines = 0
             for _x, _y in neighbor:
                 if (_x, _y) not in revealed_area:
                     i_lst = investigate_area.get(p, [])
                     i_lst.append((_x, _y))
-            if (mine_number - revealed_number) == len(investigate_area.get(p, [])):
+                    investigate_area[p] = i_lst
+                elif revealed_area[(_x, _y)][0] == 9:  # revealed_mine
+                    revealed_mines += 1
+            if (mine_number - revealed_number - revealed_mines) == len(investigate_area.get(p, [])):
                 for mine in list(investigate_area.get(p, [])):
                     revealed_area[mine] = (9, 9)
                     revealed_number += 1
                     investigate_area.pop(mine)
                 revealed_area[p] = (mine_number, revealed_number)
-        for p1 in investigate_area.keys():
-            for p2 in investigate_area.keys():
-                if p1 == p2:
-                    continue
-                
+
 
 if __name__ == "__main__":
     mines, blank_area = generate_mine_map(30, 16, 99, 10, 10, 9)
+    print(BLUE + "    " + " ".join(["%02d" % x for x in range(30)]))
     for y in range(16):
         line = [colored_mine(mines, blank_area, x, y) for x in range(30)]
-        print("\t ".join(line))
+        print("%02d" % y + ": " + " ".join(line))
+    mine_sweeper(mines, 30, 16, blank_area)
