d7e8ed135bbf2ef22a9b40eacc9728f5b39fbcd6
James Li <guoguopapa@gmail.com>
2023-01-13 03:26:23 +0800

    beta 1

--- minesweeper/mine_generator.py
+++ minesweeper/mine_generator.py
@@ -7,26 +7,178 @@ class BoomException(BaseException):
     pass
 
 
+class mine_generator(object):
+    _revealed_area: dict
+    _mines: dict
+
+    def __init__(self, width: int, height: int, mine_number: int, clicked_x: int = 0, clicked_y: int = 0,
+                 blank_size: int = 9):
+        self._width = width
+        self._height = height
+        self._mine_number = mine_number
+        self._mines, self._revealed_area = generate_mine_map(width, height, mine_number, clicked_x, clicked_y,
+                                                             blank_size)
+        self._clear_area = {}
+        self._revealed_mines = 0
+
+    def get_mine_info(self):
+        return u"%d/%d" % (self._revealed_mines, self._mine_number)
+
+    def get_squares(self):
+        return self._mines.keys()
+
+    def get_square_mines(self, p):
+        return self._mines.get(p)
+
+    def neighbor_area(self, p) -> list:
+        return neighbor_area(p[0], p[1], self._width, self._height)
+
+    def get_revealed_area(self):
+        return self._revealed_area
+
+    def get_size(self) -> tuple:
+        return self._width, self._height
+
+    def intersection_neighbor(self, p1: tuple, p2: tuple) -> tuple:
+        p1_neighbor = self.neighbor_area(p1)
+        p2_neighbor = self.neighbor_area(p2)
+        return self.intersection_area(p1_neighbor, p2_neighbor)
+
+    def intersection_area(self, area1: iter, area2: iter) -> tuple:
+        p1_p2 = {}
+        p1np2 = {}
+        p2_p1 = {}
+        for p in area1:
+            if p in area2:
+                p1np2[p] = 1
+            else:
+                p1_p2[p] = 1
+        for p in area2:
+            if p not in area1:
+                p2_p1[p] = 1
+        return p1_p2, p1np2, p2_p1
+
+    def sweep_mine(self, area: iter) -> bool:
+        print("sweep mine at %s" % str(area))
+        for mine in area:
+            self._revealed_area[mine] = 9
+            self._revealed_mines += 1
+        return len(area) > 0
+
+    def clear_safe_area(self, area: iter) -> bool:
+        unrevealed_area = [x for x in area if x not in self._revealed_area]
+        if not unrevealed_area:
+            return False
+        print("clear safe area at %s" % str(unrevealed_area))
+        updated = False
+        for pos in unrevealed_area:
+            if self.get_square_mines(pos) == 9:
+                raise BoomException("Boom!!!!! at " + str(pos))
+            self._revealed_area[pos] = self.get_square_mines(pos)
+            updated = True
+        return updated
+
+    def simple_resolve(self) -> bool:
+        print("Start to sweep mine by single square")
+        revealed_area = self._revealed_area
+        updated = False
+        for p in list(revealed_area.keys()):
+            if p in self._clear_area.keys():
+                continue
+            mine_number = revealed_area[p]
+            if mine_number == 9:
+                self._clear_area[p] = 9
+                continue
+            neighbor = self.neighbor_area(p)
+            unrevealed_neighbor = [x for x in neighbor if x not in self._revealed_area]
+            revealed_mines = [x for x in neighbor if x in self._revealed_area and self.get_square_mines(x) == 9]
+
+            if (mine_number - len(revealed_mines)) == len(unrevealed_neighbor):
+                # dig mine
+                if unrevealed_neighbor:
+                    updated = self.sweep_mine(unrevealed_neighbor)
+                # reveal safe area
+            if self.get_unrevealed_mines(p) == 0:
+                updated |= self.clear_safe_area(neighbor)
+                self._clear_area[p] = mine_number
+            if self.get_unrevealed_mines(p) == (self._mine_number - self._revealed_mines):
+                updated |= self.clear_safe_area(
+                    [x for x in self._mines.keys() if x not in self._revealed_area and x not in self.neighbor_area(p)])
+        if updated:
+            return True
+        return False
+
+    def combo_resolve(self) -> bool:
+        print("Start to sweep mine by combo square")
+        p1_lst = {}
+        updated = False
+        for p1 in list(self._revealed_area.keys()):
+            p1_lst[p1] = 1
+            m1 = self.get_unrevealed_mines(p1)
+            if m1 == 0:
+                continue
+            for p2 in list(self._revealed_area.keys()):
+                if p2 in p1_lst:
+                    continue
+                m2 = self.get_unrevealed_mines(p2)
+                if m2 == 0:
+                    continue
+                p1_p2, p1np2, p2_p1 = self.intersection_neighbor(p1, p2)
+                if not p1np2:
+                    continue
+                unrevealed_area = lambda lst: [p for p in lst if p not in self._revealed_area]
+                p1_p2_unrevealed = unrevealed_area(p1_p2)
+                p2_p1_unrevealed = unrevealed_area(p2_p1)
+                p1np1_unrevealed = unrevealed_area(p1np2)
+                mine_area = {}
+                safe_area = {}
+                if len(p1_p2_unrevealed) == m1 - m2:
+                    mine_area = p1_p2_unrevealed
+                if len(p2_p1_unrevealed) == m2 - m1:
+                    mine_area = p2_p1_unrevealed
+                if m2 <= (m1 - len(p1_p2_unrevealed)):
+                    safe_area = p2_p1_unrevealed
+                if m1 <= (m2 - len(p2_p1_unrevealed)):
+                    safe_area = p1_p2_unrevealed
+                if mine_area:
+                    updated = self.sweep_mine(mine_area)
+                if safe_area:
+                    updated |= self.clear_safe_area(safe_area)
+                if updated:
+                    return True
+        return False
+
+    def get_unrevealed_mines(self, p: tuple) -> int:
+        m = self.get_square_mines(p)
+        if m == 0 or m == 9:
+            return 0
+        for n in self.neighbor_area(p):
+            if n != p and n in self._revealed_area.keys():
+                if self._mines.get(n) == 9:
+                    m -= 1
+        return m
+
+
 def neighbor_area(clicked_x: int, clicked_y: int, width: int, height: int) -> list:
     x_area = [x for x in [clicked_x - 1, clicked_x, clicked_x + 1] if 0 <= x < width]
     y_area = [x for x in [clicked_y - 1, clicked_y, clicked_y + 1] if 0 <= x < height]
     r = [tuple(x) for x in np.array(np.meshgrid(x_area, y_area)).T.reshape(-1, 2)]
-    r.remove((clicked_x, clicked_y))
+    # r.remove((clicked_x, clicked_y))
     return r
 
 
-def expand_area(arr: np.ndarray, mines: dict, clicked_x: int, clicked_y: int, blank_area: dict = {}) -> dict:
+def expand_area(mines: dict, clicked_x: int, clicked_y: int, width, height, blank_area: dict = {}) -> dict:
     if mines[(clicked_x, clicked_y)] != 0:
         return blank_area
-    neighbor = neighbor_area(clicked_x, clicked_y, arr.shape[1], arr.shape[0])
+    neighbor = neighbor_area(clicked_x, clicked_y, width, height)
     for x, y in neighbor:
         if (x, y) != (clicked_x, clicked_y):
             if (x, y) not in blank_area:
                 blank_area[(x, y)] = 1
-                blank_area.update(expand_area(arr, mines, x, y, blank_area))
+                blank_area.update(expand_area(mines, x, y, width, height, blank_area))
         else:
             blank_area[(x, y)] = 2
-    return {p: mines[p] for p in blank_area.keys()}
+    return blank_area
 
 
 def generate_mine_map(width: int, height: int, mine_number: int, clicked_x: int = 0, clicked_y: int = 0,
@@ -53,10 +205,10 @@ def generate_mine_map(width: int, height: int, mine_number: int, clicked_x: int 
                         if arr[_y, _x]:
                             n += 1
                     mines[(x, y)] = n
-        blank_area = expand_area(arr, mines, clicked_x, clicked_y)
+        blank_area = expand_area(mines, clicked_x, clicked_y, width, height)
         if len(blank_area) < blank_size:
             continue
-        return (mines, blank_area)
+        return (mines, {p: mines[p] for p in blank_area.keys() if mines[p] != 9})
 
 
 WHITE = '\033[0m'
@@ -74,58 +226,9 @@ def colored_mine(mines, blank_area, x, y):
         return GRAY + "%02d" % (mines[(x, y)])
 
 
-def get_common_neighbor(p1_neighbor, p2_neighbor):
-    common_neighbor = []
-    for p in p1_neighbor:
-        if p in p2_neighbor:
-            common_neighbor.append(tuple(p))
-    return common_neighbor
-
-
-def mine_sweeper(mines, width, height, init_blank_area: dict):
-    revealed_area = {x: mines[x] for x in init_blank_area.keys()}
-    while True:
-        for p in list(revealed_area.keys()):
-            mine_number = revealed_area[p]
-            revealed_number = 0
-            revealed_mines = 0
-            if mine_number == 9:
-                continue
-            x, y = p
-            neighbor = neighbor_area(x, y, width, height)
-            i_lst = []
-            for _pos in neighbor:
-                pos = tuple(_pos)
-                if pos not in revealed_area:
-                    i_lst.append(pos)
-                elif revealed_area[pos] == 9:
-                    revealed_mines += 1
-            if (mine_number - revealed_mines) == len(i_lst):
-                # dig mine
-                for mine in i_lst:
-                    revealed_area[mine] = 9
-                    revealed_mines += 1
-                # reveal safe area
-            if mine_number == revealed_mines:
-                for _pos in neighbor:
-                    pos = tuple(_pos)
-                    if pos not in revealed_area:
-                        if mines[pos] == 9:
-                            raise BoomException("Boom!!!!!")
-                        revealed_area[pos] = mines[pos]
-        ## TODO: 比较相邻两个位置，标识
-        p1_lst = []
-        for p1 in list(revealed_area.keys()):
-            p1_lst.append(p1)
-            for p2 in list(revealed_area.keys()):
-                if p2 in p1_lst or not get_common_neighbor(p1, p2, width, height):
-                    continue
-
-
 if __name__ == "__main__":
     mines, blank_area = generate_mine_map(30, 16, 99, 10, 10, 9)
     print(BLUE + "    " + " ".join(["%02d" % x for x in range(30)]))
     for y in range(16):
         line = [colored_mine(mines, blank_area, x, y) for x in range(30)]
         print("%02d" % y + ": " + " ".join(line))
-    mine_sweeper(mines, 30, 16, blank_area)
--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,29 +1,32 @@
+import time
+import traceback
+
 import wx
 
-from mine_generator import generate_mine_map, neighbor_area, BoomException, get_common_neighbor
+from mine_generator import mine_generator, BoomException
 
 
 class MineSweeperDialog(wx.Dialog):
-    def __init__(self, parent):
+    def __init__(self, parent, mine_width=30, mine_height=16, mine_number=99):
         wx.Dialog.__init__(self, parent, id=wx.ID_ANY, title="Mine Sweeper", pos=wx.DefaultPosition,
                            size=wx.Size(1000, 600), style=wx.DEFAULT_DIALOG_STYLE)
         self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
-        self._mine_width = 30
-        self._mine_height = 16
-        self._mines_num = 99
-        self._revealed_mines = 0
-        self._mines = {}
+        self._mine_generator = None
+        self._mine_width = mine_width
+        self._mine_height = mine_height
+        self._mine_number = mine_number
         self._revealed_area = {}
-        self._clear_area = {}
-        self._init = False
         bSizer = wx.BoxSizer(wx.VERTICAL)
         headSizer = wx.BoxSizer(wx.HORIZONTAL)
-        self._resolveButton = wx.Button(self, wx.ID_ANY, u"DO", wx.DefaultPosition, wx.Size(64, 16))
+        self._resolveButton = wx.Button(self, wx.ID_ANY, u"Dig", wx.DefaultPosition, wx.Size(64, 32))
         self._resolveButton.Bind(wx.EVT_BUTTON, self.onResolve)
         headSizer.Add(self._resolveButton, 0, wx.ALL, 5)
-        self._mineInfoText = wx.StaticText(self, wx.ID_ANY, self.mineInfo(), wx.DefaultPosition, wx.DefaultSize,
-                                           wx.ALIGN_RIGHT)
-        headSizer.Add(self._mineInfoText, 0, wx.ALL | wx.EXPAND, 5)
+        self._refreshButton = wx.Button(self, wx.ID_ANY, u"Try Again", wx.DefaultPosition, wx.Size(64, 32))
+        self._refreshButton.Bind(wx.EVT_BUTTON, self.onTryAgain)
+        headSizer.Add(self._refreshButton, 0, wx.ALL, 5)
+        self._mineInfoText = wx.StaticText(self, wx.ID_ANY, self.mineInfo(), wx.DefaultPosition, wx.Size(600, 32),
+                                           wx.ALIGN_RIGHT | wx.ALIGN_CENTRE_VERTICAL)
+        headSizer.Add(self._mineInfoText, -1, wx.ALL | wx.EXPAND, 5)
         bSizer.Add(headSizer, 0, wx.ALL | wx.EXPAND, 5)
 
         gridSizer = wx.GridSizer(self._mine_height + 1, self._mine_width + 1, 2, 2)
@@ -50,26 +53,36 @@ class MineSweeperDialog(wx.Dialog):
         self.Bind(wx.EVT_CLOSE, self.onClose)
 
     def mineInfo(self):
-        return u"%d/%d" % (self._revealed_mines, self._mines_num)
+        if not self._mine_generator:
+            return u"0/%d" % self._mine_number
+        else:
+            return self._mine_generator.get_mine_info()
 
     def onClose(self, event):
         self.Destroy()
+
+    def onTryAgain(self, event):
+        self._mine_generator = None
+        self._revealed_area = None
+        for button in self._mine_buttons.values():
+            button.SetLabel("")
+            button.SetBackgroundColour(wx.Colour(224, 224, 224))
 
     def onClick(self, event):
         button = event.GetEventObject()
         name = button.GetName()
         x, y = [int(i) for i in name.split(':')]
-        if not self._init:
-            mines, blank_area = generate_mine_map(self._mine_width, self._mine_height, self._mines_num, x, y, 9)
-            self._mines = mines
-            self._revealed_area = blank_area
-            for p in self._mines.keys():
-                self._mine_buttons[p].SetLabel(str(self._mines[p]))
+        if not self._mine_generator:
+            self._mine_generator = mine_generator(self._mine_width, self._mine_height, self._mine_number, x, y, 9)
+            self._revealed_area = self._mine_generator.get_revealed_area().copy()
+            for p in self._mine_generator.get_squares():
+                self._mine_buttons[p].SetLabel(str(self._mine_generator.get_square_mines(p)))
             self.update()
 
     def update(self):
+        self._mineInfoText.SetLabel(self.mineInfo())
         for p in self._revealed_area:
-            if self._mines[p] != 9:
+            if self._mine_generator.get_square_mines(p) != 9:
                 self._mine_buttons[p].SetBackgroundColour(wx.Colour(250, 250, 250))
             else:
                 self._mine_buttons[p].SetBackgroundColour(wx.Colour(255, 0, 0))
@@ -79,85 +92,31 @@ class MineSweeperDialog(wx.Dialog):
         for p in area:
             if p not in self._revealed_area:
                 updated = True
-                if self._mines[p] != 9:
-                    self._mine_buttons[p].SetBackgroundColour(wx.Colour(250, 250, 250))
-                else:
-                    self._mine_buttons[p].SetBackgroundColour(wx.Colour(255, 0, 0))
+            if self._mine_generator.get_square_mines(p) != 9:
+                self._mine_buttons[p].SetBackgroundColour(wx.Colour(250, 250, 250))
+            else:
+                self._mine_buttons[p].SetBackgroundColour(wx.Colour(255, 0, 0))
         if updated:
             self._mineInfoText.SetLabel(self.mineInfo())
             self._revealed_area = area.copy()
+            if len(self._mine_generator.get_revealed_area()) == len(self._mine_generator.get_squares()):
+                wx.MessageBox("Congratulations", "Info", wx.OK | wx.ICON_INFORMATION)
         return updated
 
     def onResolve(self, event):
-        area = self._simpleResolve()
-        if not area:
-            area = self._complexResolve()
-        if area:
-            self.setRevealedArea(area)
-
-    def _simpleResolve(self):
-        revealed_area = self._revealed_area.copy()
-        mines = self._mines
-        updated = False
-        for p in list(revealed_area.keys()):
-            if p in self._clear_area.keys():
-                continue
-            mine_number = revealed_area[p]
-            revealed_mines = 0
-            if mine_number == 9:
-                self._clear_area[p] = 9
-                continue
-            x, y = p
-            neighbor = neighbor_area(x, y, self._mine_width, self._mine_height)
-            i_lst = []
-            for _pos in neighbor:
-                pos = tuple(_pos)
-                if pos not in revealed_area:
-                    i_lst.append(pos)
-                elif revealed_area[pos] == 9:
-                    revealed_mines += 1
-            if (mine_number - revealed_mines) == len(i_lst):
-                # dig mine
-                for mine in i_lst:
-                    revealed_area[mine] = 9
-                    revealed_mines += 1
-                    self._revealed_mines += 1
-                    updated = True
-                # reveal safe area
-            if mine_number == revealed_mines:
-                for _pos in neighbor:
-                    pos = tuple(_pos)
-                    if pos not in revealed_area:
-                        if mines[pos] == 9:
-                            raise BoomException("Boom!!!!!")
-                        revealed_area[pos] = mines[pos]
-                        updated = True
-                self._clear_area[p] = mine_number
-            if updated:
-                return revealed_area
-        return None
-
-        ## TODO: 比较相邻两个位置，标识
-
-    def _complexResolve(self):
-        revealed_area = self._revealed_area.copy()
-        mines = self._mines
-        updated = False
-        p_lst = []
-        for p1 in list(revealed_area.keys()):
-            if p1 in self._clear_area:
-                continue
-            p_lst.append(p1)
-            p1_neighbor = neighbor_area(p1[0], p1[1], self._mine_width, self._mine_height)
-            for p2 in list(revealed_area.keys()):
-                if p2 in p_lst or p2 in self._clear_area:
-                    continue
-                p2_neighbor = neighbor_area(p2[0], p2[1], self._mine_width, self._mine_height)
-                common_neighbor = get_common_neighbor(p1_neighbor, p2_neighbor)
-                if not common_neighbor:
-                    continue
-                    
-        return None
+        if not self._mine_generator or len(self._mine_generator.get_revealed_area()) == len(
+                self._mine_generator.get_squares()):
+            return
+        try:
+            while len(self._mine_generator.get_squares()) > len(self._mine_generator.get_revealed_area()):
+                if self._mine_generator.simple_resolve() or self._mine_generator.combo_resolve():
+                    self.setRevealedArea(self._mine_generator.get_revealed_area())
+                else:
+                    wx.MessageBox("No way out", "Info", wx.OK | wx.ICON_INFORMATION)
+                    break
+        except BoomException as e:
+            traceback.print_exc()
+            wx.MessageBox(str(e), "Error", wx.OK | wx.ICON_ERROR)
 
 
 def main():
